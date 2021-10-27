bad59ee46caa6205ba9899e20005efd79cfd4d2c
Mabelys Arias <MabelysA@gmail.com>
2021-10-27 17:12:49 -0400

    leap year problem

--- more_practice.rb
+++ more_practice.rb
@@ -698,34 +698,64 @@ p fizzbuzz(15)
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
 #                           x
 #                   i   i2
-def fibonacci(number)
-  num1, num2 = [0, 1]
+# def fibonacci(number)
+#   num1, num2 = [0, 1]
 
-  (number - 1).times do
-    num1, num2 = num2, num1 + num2
-  end
+#   (number - 1).times do
+#     num1, num2 = num2, num1 + num2
+#   end
 
-  num1
-end
+#   num1
+# end
 
-p fibonacci(9)
+# p fibonacci(9)
 
-def fibonacci(n)
-  seq = [0, 1]
+# def fibonacci(n)
+#   seq = [0, 1]
 
-  while seq.length < n
-    last_ele = seq[-1]
-    second_to_last = seq[-2]
-    next_ele = last_ele + second_to_last
-    seq << next_ele
-  end
+#   while seq.length < n
+#     last_ele = seq[-1]
+#     second_to_last = seq[-2]
+#     next_ele = last_ele + second_to_last
+#     seq << next_ele
+#   end
 
-  return seq.last
-end
+#   return seq.last
+# end
 
-p fibonacci(9) # => 21
+# p fibonacci(9) # => 21
 
 # new_r = []
 # p new_r
 # array = [0, 1]
 # p array
+# ------------------------------------------------
+#Leap Year
+
+# def leap(year)
+#   if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+#     puts "#{year} is a leap year"
+#   else
+#     puts "#{year} is not a leap year"
+#   end
+# end
+
+# leap(1997)
+# leap(1996)
+# leap(1900)
+# leap(2000)
+
+# def leap(year)
+#   if year % 400 == 0
+#     puts "#{year} is a leap year"
+#   elsif year % 4 == 0 && year % 100 != 0
+#     puts "#{year} is not a leap year"
+#   else
+#     puts "#{year} is not a leap year"
+#   end
+# end
+
+# leap(1997)
+# leap(1996)
+# leap(1900)
+# leap(2000)
